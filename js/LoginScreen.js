43c9a32edd3505113b5a12692793444fba234420
Godara <murarigodara28@gmail.com>
2022-12-14 11:52:32 -0500

    deploy ready

---
+++ js/LoginScreen.js
@@ -0,0 +1,91 @@
+
+let $ = (selector) => document.querySelector(selector);
+
+var modal = document.getElementById('id01');
+
+window.onload = function () {
+
+  
+  $('#login').onclick = loginValidate;
+  $('#eye').onclick = passwordVisible;
+  $('#email').onchange = valuesChange;
+  $('#password').onchange = valuesChange;
+ 
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function goToHome()
+{
+  window.location.href = "../index.html";
+}
+
+
+
+function valuesChange()
+{
+  let emailValFlag = emailValidate();
+  let passCheck = valiatePassword();
+  if (emailValFlag && passCheck) {
+    $('#login').style.opacity = 1
+  }
+}
+
+function passwordVisible(){
+ 
+  this.classList.toggle("fa-eye-slash")
+  const type = $('#password').getAttribute("type") === "password" ? "text" : "password"
+  $('#password').setAttribute("type", type)
+}
+                    
+
+
+
+
+  function loginValidate(e) {
+
+    e.preventDefault();
+   
+    let emailValFlag = emailValidate();
+    if (!emailValFlag) {
+        document.getElementById('err-email').style.display = 'block';
+    }
+    
+    let passCheck = valiatePassword();
+    if (!passCheck) {
+        document.getElementById('err-passwd').style.display = 'block';
+    }
+    
+    if (emailValFlag && passCheck) {
+      localStorage.setItem('email',document.getElementById('email').value)
+      document.getElementById('id01').style.display='block'
+    }else{
+    
+    }
+
+}
+
+
+//phone number validate
+function emailValidate() {
+   
+    const ree = /\S+@\S+\.\S+/;
+    let iemail = document.getElementById('email').value;
+    if (ree.test(iemail)) {
+        return true;
+    }
+    return false;
+}
+
+//password Validator
+function valiatePassword() {
+    if (document.getElementById('password').value.length != 0 ) {
+        return true;
+    }
+    return false;
+}
+
+;
+
+
